ece6f90d69e39307dc4d173ab428e2ce70b4e8c9
ryanozx <59072730+ryanozx@users.noreply.github.com>
2021-04-12 00:47:27 +0800

    Reformatted Exercises 1.1-1.5.rkt

--- Exercises 1.1-1.5.rkt
+++ Exercises 1.1-1.5.rkt
@@ -44,9 +44,9 @@ larger numbers. |#
 #| Exercise 1.4: Observe that our model of evaluation allows
 for combinations whose operators are compound expressions.
 Use this observation to describe the behavior of the
-following procedure:
+following procedure: |#
 (define (a-plus-abs-b a b)
-  ((if (> b 0) + -) a b)) |#
+  ((if (> b 0) + -) a b))
 
 #| The operator (if (> b 0) + -) along with the operands a and b are first evaluated.
 Since the operator involves the if-conditional, the predicate (> b 0) is first evaluated.
