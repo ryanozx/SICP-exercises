c557e2a33a70a992419d5691dd68907244421f0e
ryanozx <59072730+ryanozx@users.noreply.github.com>
2021-04-12 00:46:46 +0800

    Reformatted Exercises 1.1-1.5.rkt

--- Exercises 1.1-1.5.rkt
+++ Exercises 1.1-1.5.rkt
@@ -63,13 +63,13 @@ the result returned as the result of a-plus-abs-b. |#
 #| Exercise 1.5: Ben Bitdiddle has invented a test to determine
 whether the interpreter he is faced with is using applicative-
 order evaluation or normal-order evaluation. He defines the
-following two procedures:
+following two procedures: |#
 (define (p) (p))
 (define (test x y)
 (if (= x 0) 0 y))
-Then he evaluates the expression
+#| Then he evaluates the expression |#
 (test 0 (p))
-What behavior will Ben observe with an interpreter that
+#| What behavior will Ben observe with an interpreter that
 uses applicative-order evaluation? What behavior will he
 observe with an interpreter that uses normal-order evaluation?
 Explain your answer. (Assume that the evaluation rule for the special form
